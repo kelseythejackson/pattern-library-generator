c3106bd78229ca72835226cf8e594c1cad4519b5
Kelsey Jackson <kelseythej@gmail.com>
2019-11-25 19:28:18 -0600

    WIP Project Setup
    Continues to set up project

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,34 @@
-import fs from 'fs-extra'
+import fse from 'fs-extra'
 import path from 'path'
+import ejs from 'ejs'
+import { promisify } from 'util'
+import glob from 'glob'
 
-console.log('hello')
+const ejsRenderFile = promisify(ejs.renderFile)
+const globP = promisify(glob)
+
+const srcPath = './src'
+const distPath = './public'
+
+fse.emptyDirSync(distPath)
+
+
+globP('**/*.ejs', { cwd: `${srcPath}/pages` })
+  .then((files) => {
+    files.forEach((file) => {
+      const fileData = path.parse(file)
+      const destPath = path.join(distPath, fileData.dir)
+
+      fse.mkdirs(destPath)
+      .then(() => {
+        return ejsRenderFile(`${srcPath}/pages/${file}`)
+      })
+      .then((pageContents) => {
+        return ejsRenderFile(`${srcPath}/layout.ejs`)
+      })
+      .then((layoutContent) => {
+        fse.writeFile(`${destPath}/${fileData.name}.html`, layoutContent)
+      })
+      console.log(destPath)
+    })
+  })
